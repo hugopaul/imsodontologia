a86b45f6df9c3d536f4f90a723a7670101f1e905
Hugo Paul <hugo.4500@icloud.com>
2020-11-04 22:23:55 -0300

    CRIAÇÃO DO PROJETO DA CLINICA IMS ODONTOLOGIA E ESTETICA

---
+++ src/main/java/br/com/imsodontologia/imsodontologia/ImsodontologiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.imsodontologia.imsodontologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsodontologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsodontologiaApplication.class, args);
+	}
+
+}
